32ce1ed9d38cb034a8a5ab1d73cbecf89d05faf5
Davetimmer <dave.timmer@hotmail.com>
2017-03-21 21:52:16 +0100

    Added the logo, modified football-files

--- files/js/football-api.js
+++ files/js/football-api.js
@@ -1,30 +1,226 @@
 var teams = [338, 62, 354, 65, 66, 57, 64, 61, 5, 4, 674, 675, 678, 524, 548, 559, 86, 78, 81, 109, 100, 113];
-var teamNumber = teams[Math.floor(Math.random() * (teams.length + 1))];
+var totalScore = 0;
 
-$.ajax({
-	headers: { 'X-Auth-Token': '9221c7d370d8437a8b131dc147f509e1' },
-	url: "http://api.football-data.org/v1/teams/" + teamNumber + "/players",
-	dataType: 'json',
-	type: 'GET',
-}).done(function(response) {
-	var number = 1 + Math.floor(Math.random() * response.count);
-	var playerName = response.players[number].name;
-	var jerseyNumber = response.players[number].jerseyNumber;
-	var playerPosition = response.players[number].position;
-	$("#position").append(playerPosition);
-	var playerNationality = response.players[number].nationality;
-	$("#nationality").append(playerNationality);
-	var playerMarketValue = response.players[number].marketValue;
-	$("#market-value").append(playerMarketValue);
-	console.log(playerName, playerPosition, playerNationality, playerMarketValue);
-	$("#answer").submit(function() {
-		if ($("#answer").val().toLowerCase() === playerName.toLowerCase()) {
-		console.log("Congratulations");
-		} else {
-		console.log("WRONG!");
-		};
+$(document).ready(function() {
+
+	var teamNumber = teams[Math.floor(Math.random() * (teams.length + 1))];
+	$.ajax({
+		headers: { 'X-Auth-Token': '9221c7d370d8437a8b131dc147f509e1' },
+		url: "http://api.football-data.org/v1/teams/" + teamNumber + "/players",
+		dataType: 'json',
+		type: 'GET',
+	}).done(function(response) {
+		var number = 1 + Math.floor(Math.random() * response.count);
+		var playerName = response.players[number].name;
+		var jerseyNumber = response.players[number].jerseyNumber;
+		var playerPosition = response.players[number].position;
+		$("#position1").append(playerPosition);
+		var playerNationality = response.players[number].nationality;
+		$("#nationality1").append(playerNationality);
+		var playerMarketValue = response.players[number].marketValue;
+		$("#market-value1").append(playerMarketValue);
+		console.log(playerName, playerPosition, playerNationality, playerMarketValue);
+		$("#answer1").submit(function() {
+			if ($("#answer1").val().toLowerCase() === playerName.toLowerCase()) {
+			console.log("Congratulations");
+			} else {
+			console.log("WRONG!");
+			};
+		return false;	
+		});
+	}); 
+
+	var teamNumber = teams[Math.floor(Math.random() * (teams.length + 1))];
+	$.ajax({
+		headers: { 'X-Auth-Token': '9221c7d370d8437a8b131dc147f509e1' },
+		url: "http://api.football-data.org/v1/teams/" + teamNumber + "/players",
+		dataType: 'json',
+		type: 'GET',
+	}).done(function(response) {
+		var number = 1 + Math.floor(Math.random() * response.count);
+		var playerName = response.players[number].name;
+		var jerseyNumber = response.players[number].jerseyNumber;
+		var playerPosition = response.players[number].position;
+		$("#position2").append(playerPosition);
+		var playerNationality = response.players[number].nationality;
+		$("#nationality2").append(playerNationality);
+		var playerMarketValue = response.players[number].marketValue;
+		$("#market-value2").append(playerMarketValue);
+		console.log(playerName, playerPosition, playerNationality, playerMarketValue);
+
+		var userAnswer = document.getElementById("answer2").value();
+		$("#answer2").submit(function() {
+			if (userAnswer.toLowerCase() === playerName.toLowerCase()) {
+				totalScore += 1;
+				alert("Congrats!");
+				// console.log(totalScore);
+			} else {
+				totalScore = totalScore;
+				// console.log(totalScore);
+			};
+		return false;
+		});
 	});
-}); 
+
+	var teamNumber = teams[Math.floor(Math.random() * (teams.length + 1))];
+	$.ajax({
+		headers: { 'X-Auth-Token': '9221c7d370d8437a8b131dc147f509e1' },
+		url: "http://api.football-data.org/v1/teams/" + teamNumber + "/players",
+		dataType: 'json',
+		type: 'GET',
+	}).done(function(response) {
+		var number = 1 + Math.floor(Math.random() * response.count);
+		var playerName = response.players[number].name;
+		var jerseyNumber = response.players[number].jerseyNumber;
+		var playerPosition = response.players[number].position;
+		$("#position3").append(playerPosition);
+		var playerNationality = response.players[number].nationality;
+		$("#nationality3").append(playerNationality);
+		var playerMarketValue = response.players[number].marketValue;
+		$("#market-value3").append(playerMarketValue);
+		console.log(playerName, playerPosition, playerNationality, playerMarketValue);
+	});
+
+	var teamNumber = teams[Math.floor(Math.random() * (teams.length + 1))];
+	$.ajax({
+		headers: { 'X-Auth-Token': '9221c7d370d8437a8b131dc147f509e1' },
+		url: "http://api.football-data.org/v1/teams/" + teamNumber + "/players",
+		dataType: 'json',
+		type: 'GET',
+	}).done(function(response) {
+		var number = 1 + Math.floor(Math.random() * response.count);
+		var playerName = response.players[number].name;
+		var jerseyNumber = response.players[number].jerseyNumber;
+		var playerPosition = response.players[number].position;
+		$("#position4").append(playerPosition);
+		var playerNationality = response.players[number].nationality;
+		$("#nationality4").append(playerNationality);
+		var playerMarketValue = response.players[number].marketValue;
+		$("#market-value4").append(playerMarketValue);
+		console.log(playerName, playerPosition, playerNationality, playerMarketValue);
+	});
+
+	var teamNumber = teams[Math.floor(Math.random() * (teams.length + 1))];
+	$.ajax({
+		headers: { 'X-Auth-Token': '9221c7d370d8437a8b131dc147f509e1' },
+		url: "http://api.football-data.org/v1/teams/" + teamNumber + "/players",
+		dataType: 'json',
+		type: 'GET',
+	}).done(function(response) {
+		var number = 1 + Math.floor(Math.random() * response.count);
+		var playerName = response.players[number].name;
+		var jerseyNumber = response.players[number].jerseyNumber;
+		var playerPosition = response.players[number].position;
+		$("#position5").append(playerPosition);
+		var playerNationality = response.players[number].nationality;
+		$("#nationality5").append(playerNationality);
+		var playerMarketValue = response.players[number].marketValue;
+		$("#market-value5").append(playerMarketValue);
+		console.log(playerName, playerPosition, playerNationality, playerMarketValue);
+	});
+
+	var teamNumber = teams[Math.floor(Math.random() * (teams.length + 1))];
+	$.ajax({
+		headers: { 'X-Auth-Token': '9221c7d370d8437a8b131dc147f509e1' },
+		url: "http://api.football-data.org/v1/teams/" + teamNumber + "/players",
+		dataType: 'json',
+		type: 'GET',
+	}).done(function(response) {
+		var number = 1 + Math.floor(Math.random() * response.count);
+		var playerName = response.players[number].name;
+		var jerseyNumber = response.players[number].jerseyNumber;
+		var playerPosition = response.players[number].position;
+		$("#position6").append(playerPosition);
+		var playerNationality = response.players[number].nationality;
+		$("#nationality6").append(playerNationality);
+		var playerMarketValue = response.players[number].marketValue;
+		$("#market-value6").append(playerMarketValue);
+		console.log(playerName, playerPosition, playerNationality, playerMarketValue);
+	});
+
+	var teamNumber = teams[Math.floor(Math.random() * (teams.length + 1))];
+	$.ajax({
+		headers: { 'X-Auth-Token': '9221c7d370d8437a8b131dc147f509e1' },
+		url: "http://api.football-data.org/v1/teams/" + teamNumber + "/players",
+		dataType: 'json',
+		type: 'GET',
+	}).done(function(response) {
+		var number = 1 + Math.floor(Math.random() * response.count);
+		var playerName = response.players[number].name;
+		var jerseyNumber = response.players[number].jerseyNumber;
+		var playerPosition = response.players[number].position;
+		$("#position7").append(playerPosition);
+		var playerNationality = response.players[number].nationality;
+		$("#nationality7").append(playerNationality);
+		var playerMarketValue = response.players[number].marketValue;
+		$("#market-value7").append(playerMarketValue);
+		console.log(playerName, playerPosition, playerNationality, playerMarketValue);
+	});
+
+	var teamNumber = teams[Math.floor(Math.random() * (teams.length + 1))];
+	$.ajax({
+		headers: { 'X-Auth-Token': '9221c7d370d8437a8b131dc147f509e1' },
+		url: "http://api.football-data.org/v1/teams/" + teamNumber + "/players",
+		dataType: 'json',
+		type: 'GET',
+	}).done(function(response) {
+		var number = 1 + Math.floor(Math.random() * response.count);
+		var playerName = response.players[number].name;
+		var jerseyNumber = response.players[number].jerseyNumber;
+		var playerPosition = response.players[number].position;
+		$("#position8").append(playerPosition);
+		var playerNationality = response.players[number].nationality;
+		$("#nationality8").append(playerNationality);
+		var playerMarketValue = response.players[number].marketValue;
+		$("#market-value8").append(playerMarketValue);
+		console.log(playerName, playerPosition, playerNationality, playerMarketValue);
+	});
+
+	var teamNumber = teams[Math.floor(Math.random() * (teams.length + 1))];
+	$.ajax({
+		headers: { 'X-Auth-Token': '9221c7d370d8437a8b131dc147f509e1' },
+		url: "http://api.football-data.org/v1/teams/" + teamNumber + "/players",
+		dataType: 'json',
+		type: 'GET',
+	}).done(function(response) {
+		var number = 1 + Math.floor(Math.random() * response.count);
+		var playerName = response.players[number].name;
+		var jerseyNumber = response.players[number].jerseyNumber;
+		var playerPosition = response.players[number].position;
+		$("#position9").append(playerPosition);
+		var playerNationality = response.players[number].nationality;
+		$("#nationality9").append(playerNationality);
+		var playerMarketValue = response.players[number].marketValue;
+		$("#market-value9").append(playerMarketValue);
+		console.log(playerName, playerPosition, playerNationality, playerMarketValue);
+	});
+
+	var teamNumber = teams[Math.floor(Math.random() * (teams.length + 1))];
+	$.ajax({
+		headers: { 'X-Auth-Token': '9221c7d370d8437a8b131dc147f509e1' },
+		url: "http://api.football-data.org/v1/teams/" + teamNumber + "/players",
+		dataType: 'json',
+		type: 'GET',
+	}).done(function(response) {
+		var number = 1 + Math.floor(Math.random() * response.count);
+		var playerName = response.players[number].name;
+		var jerseyNumber = response.players[number].jerseyNumber;
+		var playerPosition = response.players[number].position;
+		$("#position10").append(playerPosition);
+		var playerNationality = response.players[number].nationality;
+		$("#nationality10").append(playerNationality);
+		var playerMarketValue = response.players[number].marketValue;
+		$("#market-value10").append(playerMarketValue);
+		console.log(playerName, playerPosition, playerNationality, playerMarketValue);
+	});
+
+});
+
+$(document).ready(function() {
+	$(".panel-header").click(function() {
+		$(".panel-body", this).hide();
+	});	
+});
 
 // Teams: 
 // 338 Leicester
